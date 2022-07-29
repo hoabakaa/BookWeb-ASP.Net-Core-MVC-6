e1a4c855000b2eeddc998fe9ddf4c0cafb8c2aaa
Cao Nhat Long <longcnhe141293@gmail.com>
2022-07-29 15:35:03 +0700

    add connect database

---
+++ BookWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
